91d900428979a34801aee85841e1581b8fce37c2
Prawin Kiritharan <prawinkiritharan@gmail.com>
2023-09-25 10:38:38 +0100

    made changes to demo.py

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-test1
 
 Hello, This is my first edit to README.md.
+
--- demo.py
+++ demo.py
@@ -1,2 +1,4 @@
 if __name__ ==  "__main__":
-    print("Hello World")
+    name = input("Enter your name: ")
+    name = name.title()
+    print("Hello", name)
